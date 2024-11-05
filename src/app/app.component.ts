8293591ff592a81543e626041ea3a727c49a8a20
kevinquintanah12 <kevinherreraq12@gmail.com>
2024-11-05 10:48:00 -0600

    Implementar header con botones de inicio de sesión y registro con diseño moderno; incluye header con botones funcionales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
 import { SeccionInicioComponent } from './seccion-inicio/seccion-inicio.component';
 import { MatToolbarModule } from '@angular/material/toolbar'; // Importa MatToolbarModule
 
@@ -7,11 +9,12 @@ import { MatToolbarModule } from '@angular/material/toolbar'; // Importa MatTool
   selector: 'app-root',
   standalone: true,
   template: `
-   
+    <app-header></app-header> <!-- Incluye tu componente de seccion -->
+
     <app-seccion-inicio></app-seccion-inicio> <!-- Incluye tu componente de seccion -->
     <router-outlet></router-outlet> <!-- Este es el lugar donde se renderizarán las rutas -->
   `,
-  imports: [RouterOutlet, SeccionInicioComponent, MatToolbarModule] // Importa MatToolbarModule aquí
+  imports: [RouterOutlet, SeccionInicioComponent, MatToolbarModule,HeaderComponent] // Importa MatToolbarModule aquí
 })
 export class AppComponent {
   title = 'CPM System';
